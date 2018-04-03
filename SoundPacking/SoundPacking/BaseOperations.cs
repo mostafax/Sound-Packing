7a15a435c1fca8d4b31df82b8b68e052fda3efb1
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2018-04-03 18:19:14 +0200

    BaseOperations Abstract

---
+++ SoundPacking/SoundPacking/BaseOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FileDescription
+{
+    public string Name;
+    public int Duration;
+}
+
+class FolderDescription
+{
+    public string Name;
+    public int MaxLength, CurrentLength;
+}
+
+static class BaseOperations
+{
+    //private
+    static private List<FileDescription> files;
+    static private List<FolderDescription> folders;
+    //public
+    static public List<FileDescription> Files
+    {
+        get
+        {
+            return files;
+        }
+    }
+    static public List<FolderDescription> Folders
+    {
+        get
+        {
+            return folders;
+        }
+    }
+    static public void Initlaize()
+    {
+        throw new NotImplementedException();
+    }
+    static public int ConstructFolder()
+    {
+        throw new NotImplementedException();
+    }
+    static public void MoveFile(int SouceIndex, int DestinationIndex)
+    {
+        throw new NotImplementedException();
+    }
+}
--- SoundPacking/SoundPacking/Program.cs
+++ SoundPacking/SoundPacking/Program.cs
@@ -10,6 +10,7 @@ namespace SoundPacking
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
